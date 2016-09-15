ea6ba439aae5643815b4f600bbf61cac3fb55110
architect <4rchit3ct@gmx.de>
2016-09-15 20:29:17 +0200

    rename package to modules

---
+++ src/main/java/org/panthercode/arctic/core/processing/modules/Process.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2016 PantherCode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.panthercode.arctic.core.processing.modules;
+
+import org.panthercode.arctic.core.helper.identity.Identity;
+import org.panthercode.arctic.core.helper.identity.annotation.IdentityInfo;
+import org.panthercode.arctic.core.helper.version.annotation.VersionInfo;
+import org.panthercode.arctic.core.processing.ProcessState;
+import org.panthercode.arctic.core.settings.context.Context;
+
+import java.util.Iterator;
+
+/**
+ * The Process class runs one element after another.
+ */
+@IdentityInfo(name = "Standard Process", group = "Process Module")
+@VersionInfo(major = 1)
+public class Process extends Bundle {
+
+    /**
+     * actual running module
+     */
+    private Module currentModule = null;
+
+    /**
+     * Standard Constructor
+     */
+    public Process() {
+        super();
+    }
+
+    /**
+     * Constructor
+     *
+     * @param context context the module is associated with.
+     */
+    public Process(Context context) {
+        super(context);
+    }
+
+    /**
+     * Constructor
+     *
+     * @param identity identity the module is associated with.
+     * @param context  context the module is associated with.
+     */
+    public Process(Identity identity, Context context) {
+        super(identity, context);
+    }
+
+    /**
+     * Copy constructor
+     *
+     * @param process object to copy
+     */
+    public Process(Process process)
+            throws CloneNotSupportedException, NullPointerException {
+        super(process);
+    }
+
+    /**
+     * Returns a actual running module.
+     *
+     * @return Returns a actual running module or null if process doesn't run.
+     */
+    public Module getCurrentModule() {
+        return this.currentModule;
+    }
+
+    /**
+     * Starts the process.
+     *
+     * @trows Exception Is eventually thrown by actual module or if an error occurred while running process.
+     */
+    @Override
+    public void start()
+            throws Exception {
+        super.start();
+
+        try {
+            Iterator<Module> iterator = this.iterator();
+            while (iterator.hasNext() && this.isRunning()) {
+                this.currentModule = iterator.next();
+
+                this.currentModule.start();
+
+                if (!this.currentModule.isSucceeded()) {
+                    this.currentModule = null;
+
+                    //Todo: return false for whole process instead of throwing an exception
+                    throw new RuntimeException("Step does not return successful.");
+                }
+            }
+
+            this.currentModule = null;
+
+            this.changeState(ProcessState.SUCCEEDED);
+        } catch (Exception e) {
+            this.changeState(ProcessState.FAILED);
+            //Todo: throw exception if it's not ignored.
+        }
+    }
+
+    /**
+     * Stops the actual process. It's not guaranteed that process stops immediately, but after finishing actual module.
+     * Calls actual module's <tt>stop()</tt> method.
+     *
+     * @trows Exception Is eventually thrown by actual module or if an error occurred while stopping process.
+     */
+    public void stop()
+            throws Exception {
+        if (this.currentModule != null) {
+            this.currentModule.stop();
+        }
+
+        if (this.canChangeState(ProcessState.STOPPED)) {
+            super.stop();
+        }
+    }
+
+    /**
+     * Creates a copy of this object.
+     *
+     * @return Return a copy of this object.
+     * @throws CloneNotSupportedException Is thrown if child element doesn't support cloning.
+     */
+    @Override
+    public Process clone()
+            throws CloneNotSupportedException, NullPointerException {
+        return new Process(this);
+    }
+
+    /**
+     * Returns a hash code value of this object.
+     *
+     * @return Returns a hash code value of this object.
+     */
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    /**
+     * Checks if this object is equals to another one.
+     *
+     * @param obj other object for comparison
+     * @return Returns <code>true</code> if both objects are equal; Otherwise <tt>false</tt>.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof Process)) {
+            return false;
+        }
+
+        Process process = (Process) obj;
+
+        return super.equals(process);
+    }
+}
