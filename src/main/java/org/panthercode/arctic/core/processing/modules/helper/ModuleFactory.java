2e5ec39c42b4c12767c265ee3e271659b1ccc79b
architect <4rchit3ct@gmx.de>
2016-10-03 21:37:35 +0200

    modify ModuleFactory class

--- src/main/java/org/panthercode/arctic/core/processing/modules/helper/ModuleFactory.java
+++ src/main/java/org/panthercode/arctic/core/processing/modules/helper/ModuleFactory.java
@@ -18,6 +18,7 @@ package org.panthercode.arctic.core.processing.modules.helper;
 import org.panthercode.arctic.core.arguments.ArgumentUtils;
 import org.panthercode.arctic.core.helper.identity.Identity;
 import org.panthercode.arctic.core.processing.modules.Module;
+import org.panthercode.arctic.core.processing.modules.annotation.RootModule;
 import org.panthercode.arctic.core.reflect.ClassBuilder;
 import org.panthercode.arctic.core.reflect.ClassUtils;
 import org.panthercode.arctic.core.settings.context.Context;
@@ -28,117 +29,26 @@ import java.util.List;
 
 //TODO: update implementation
 //TODO: documentation
+
 /**
  *
  */
 public class ModuleFactory {
+
     /**
      *
      */
     private ModuleFactory() {
     }
 
-    /**
-     *
-     * @param tClass
-     * @param name
-     * @param group
-     * @param <T>
-     * @return
-     * @throws InvocationTargetException
-     * @throws NoSuchMethodException
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     */
-    public static <T extends Module> Module create(Class<T> tClass, String name, String group)
-            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        return ModuleFactory.create(tClass, name, group, null);
-    }
-
-    /**
-     *
-     * @param tClass
-     * @param name
-     * @param group
-     * @param context
-     * @param <T>
-     * @return
-     * @throws NoSuchMethodException
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     * @throws InvocationTargetException
-     */
-    public static <T extends Module> Module create(Class<T> tClass, String name, String group, Context context)
-            throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-        return ModuleFactory.create(tClass, Identity.generate(name, group), context);
-    }
-
-    /**
-     *
-     * @param tClass
-     * @param identity
-     * @param <T>
-     * @return
-     * @throws NoSuchMethodException
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     * @throws InvocationTargetException
-     */
-    public static <T extends Module> Module create(Class<T> tClass, Identity identity)
-            throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-        return ModuleFactory.create(tClass, identity, null);
-    }
-
-    /**
-     *
-     * @param tClass
-     * @param identity
-     * @param context
-     * @param <T>
-     * @return
-     * @throws InvocationTargetException
-     * @throws NoSuchMethodException
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     */
-    public static <T extends Module> Module create(Class<T> tClass, Identity identity, Context context)
-            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        ArgumentUtils.assertNotNull(identity, "identity");
-
-        Module module = ClassBuilder.create(tClass).setArguments(identity).build();
-
-        if (context != null) {
-            module.setContext(context);
-        }
-
-        return module;
-    }
-
-    /**
-     *
-     * @param path
-     * @param name
-     * @param group
-     * @param <T>
-     * @return
-     * @throws NoSuchMethodException
-     * @throws IOException
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     * @throws InvocationTargetException
-     * @throws ClassNotFoundException
-     */
-    public static <T extends Module> Module create(String path, String name, String group)
+    public static <T extends Module> T create(String path)
             throws NoSuchMethodException, IOException, InstantiationException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
-        return ModuleFactory.create(path, name, group, null);
+        return ModuleFactory.create(path, null);
     }
 
     /**
-     *
      * @param path
-     * @param name
-     * @param group
-     * @param context
+     * @param
      * @param <T>
      * @return
      * @throws NoSuchMethodException
@@ -148,31 +58,12 @@ public class ModuleFactory {
      * @throws InvocationTargetException
      * @throws ClassNotFoundException
      */
-    public static <T extends Module> Module create(String path, String name, String group, Context context)
+    public static <T extends Module> T create(String path, Context context)
             throws NoSuchMethodException, IOException, InstantiationException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
-        return ModuleFactory.create(path, Identity.generate(name, group), context);
+        return ModuleFactory.create(path, null, context);
     }
 
     /**
-     *
-     * @param path
-     * @param identity
-     * @param <T>
-     * @return
-     * @throws NoSuchMethodException
-     * @throws IOException
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     * @throws InvocationTargetException
-     * @throws ClassNotFoundException
-     */
-    public static <T extends Module> Module create(String path, Identity identity)
-            throws NoSuchMethodException, IOException, InstantiationException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
-        return ModuleFactory.create(path, identity, null);
-    }
-
-    /**
-     *
      * @param path
      * @param identity
      * @param context
@@ -185,23 +76,73 @@ public class ModuleFactory {
      * @throws IllegalAccessException
      * @throws InvocationTargetException
      */
-    public static <T extends Module> Module create(String path, Identity identity, Context context)
+    public static <T extends Module> T create(String path, Identity identity, Context context)
             throws IOException, ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
         ArgumentUtils.assertNotNull(path, "path");
         ArgumentUtils.assertNotNull(identity, "identity");
 
-        List<Class<?>> classes = ClassUtils.getClassesFromJarFile(path, Module.class);
+        //TODO: extend ClassUtils with annotation filter
+        List<Class<?>> classes = ClassUtils.getClassesFromJarFile(path, RootModule.class);
 
         if (classes.isEmpty()) {
-            throw new ClassNotFoundException("");//TODO: find no module class in jar file
+            throw new ClassNotFoundException("The file doesn't contains a root element.");
         }
 
         if (classes.size() > 1) {
-            throw new RuntimeException(""); //TODO: there are more than one possibilities.
+            throw new RuntimeException("The file contains more than one root element.");
         }
 
-        Class<Module> moduleClass = (Class<Module>) classes.get(0);
+        Class<T> moduleClass = (Class<T>) classes.get(0);
+
+        //TODO: check annotation of class
 
         return ModuleFactory.create(moduleClass, identity, context);
+    }
+
+    /**
+     * @param tClass
+     * @param <T>
+     * @return
+     * @throws InvocationTargetException
+     * @throws NoSuchMethodException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+    public static <T extends Module> T create(Class<T> tClass)
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        return ModuleFactory.create(tClass, null);
+    }
+
+    /**
+     * @param tClass
+     * @param
+     * @param <T>
+     * @return
+     * @throws NoSuchMethodException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    public static <T extends Module> T create(Class<T> tClass, Context context)
+            throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        return ModuleFactory.create(tClass, null, context);
+    }
+
+    /**
+     * @param tClass
+     * @param identity
+     * @param context
+     * @param <T>
+     * @return
+     * @throws InvocationTargetException
+     * @throws NoSuchMethodException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+    public static <T extends Module> T create(Class<T> tClass, Identity identity, Context context)
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        ArgumentUtils.assertNotNull(identity, "identity");
+
+        return ClassBuilder.create(tClass).setArguments(identity, context).build();
     }
 }
