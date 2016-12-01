f46e774170e3feb3924064d3ad60e2367207f718
architect <4rchit3ct@gmx.de>
2016-12-01 23:43:41 +0100

    implement test cases

--- src/test/java/org/panthercode/arctic/core/helper/unit/BinaryMemoryUnitTest.java
+++ src/test/java/org/panthercode/arctic/core/helper/unit/BinaryMemoryUnitTest.java
@@ -1,9 +1,34 @@
 package org.panthercode.arctic.core.helper.unit;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * Created by architect on 01.12.16.
  */
 public class BinaryMemoryUnitTest {
+
+    public static final double TEST_DOUBLE_ONE = 1.0;
+
+    public static final double TEST_DOUBLE_VALUE = 1024.0;
+
+    public static final BinaryMemoryUnit TEST_UNIT_MEBIBYTE = BinaryMemoryUnit.MEBIBYTE;
+
+    public static final BinaryMemoryUnit TEST_UNIT_GIBIBYTE = BinaryMemoryUnit.GIBIBYTE;
+
+    public static final BinaryMemoryUnit TEST_UNIT_TEBIBYTE = BinaryMemoryUnit.TEBIBYTE;
+
+    @Test
+    public void T01_BinaryMemoryUnit_valueOf() {
+        BinaryMemoryUnit actualUnit = BinaryMemoryUnit.valueOf(TEST_DOUBLE_VALUE, TEST_UNIT_MEBIBYTE);
+
+        Assert.assertEquals(actualUnit, TEST_UNIT_GIBIBYTE, "Units are equal.");
+    }
+
+    @Test
+    public void T02_BinaryMemoryUnit_toMebiByte() {
+        double actualValue = TEST_UNIT_TEBIBYTE.toPebiByte(TEST_DOUBLE_VALUE);
+
+        Assert.assertEquals(actualValue, TEST_DOUBLE_ONE, "Values are equal.");
+    }
 }
--- src/test/java/org/panthercode/arctic/core/helper/unit/MemoryUnitTest.java
+++ src/test/java/org/panthercode/arctic/core/helper/unit/MemoryUnitTest.java
@@ -1,9 +1,34 @@
 package org.panthercode.arctic.core.helper.unit;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * Created by architect on 01.12.16.
  */
 public class MemoryUnitTest {
+
+    public static final double TEST_DOUBLE_ONE = 1.0;
+
+    public static final double TEST_DOUBLE_VALUE = 1000.0;
+
+    public static final MemoryUnit TEST_UNIT_PETABYTE = MemoryUnit.PETABYTE;
+
+    public static final MemoryUnit TEST_UNIT_ZETTABYTE = MemoryUnit.ZETTABYTE;
+
+    public static final MemoryUnit TEST_UNIT_YOTTABYTE = MemoryUnit.YOTTABYTE;
+
+    @Test
+    public void T01_MemoryUnit_valueOf() {
+        MemoryUnit actualUnit = MemoryUnit.valueOf(TEST_DOUBLE_VALUE, TEST_UNIT_ZETTABYTE);
+
+        Assert.assertEquals(actualUnit, TEST_UNIT_YOTTABYTE, "Units are equal.");
+    }
+
+    @Test
+    public void T02_MemoryUnit_toExaByte() {
+        double actualValue = TEST_UNIT_PETABYTE.toExaByte(TEST_DOUBLE_VALUE);
+
+        Assert.assertEquals(actualValue, TEST_DOUBLE_ONE, "Values are equal.");
+    }
 }
